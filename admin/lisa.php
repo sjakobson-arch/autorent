da14291e61b53cf087e920a43bea6ee8f2d48431
sjakobson <sjakobson@hkhk.edu.ee>
2026-04-10 09:06:28 +0300

    4653454

--- admin/lisa.php
+++ admin/lisa.php
@@ -7,7 +7,21 @@
     <form action="lisa.php" method="get">
         <div class="row g-4">
             <div class="col-sm-6">
-                <label for="mark" class="form"></label>
+                <label for="mark" class="form-label">Mark</label>
+                <input type="text" class="form-control" id="mark" name="mark">
+
+                <label for="model" class="form-label">Mark</label>
+                <input type="text" class="form-control" id="model" name="model">
+
+                <label for="engine" class="form-label">Mark</label>
+                <input type="text" class="form-control" id="engine" name="engine">
+
+                <label for="fuel" class="form-label">Mark</label>
+                <input type="text" class="form-control" id="fuel" name="fuel">
+                
+                <label for="price" class="form-label">Mark</label>
+                <input type="text" class="form-control" id="price" name="price">
+
 
 
             </div> 
