43cde7f10b422b17897d0c038805da2bc954faac
sjakobson <sjakobson@hkhk.edu.ee>
2026-04-10 08:59:45 +0300

    lisa

---
+++ admin/lisa.php
@@ -0,0 +1,23 @@
+<?php include('../config.php'); ?>
+<?php include('../header.php'); ?>
+
+<!-- sisu -->
+<div class="container">
+    <h2>Auto lisamine</h2>
+    <form action="lisa.php" method="get">
+        <div class="row g-4">
+            <div class="col-sm-6">
+                <label for="mark" class="form"></label>
+
+
+            </div> 
+        </div>
+
+
+
+    </form>
+
+
+
+
+</div>
