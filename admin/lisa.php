8831b971398657f46efce3bbc57dd3c6e61871c1
sjakobson <sjakobson@hkhk.edu.ee>
2026-04-10 09:22:48 +0300

    wwwwwwwwwww

--- admin/lisa.php
+++ admin/lisa.php
@@ -13,17 +13,31 @@
                 <label for="model" class="form-label">Model</label>
                 <input type="text" class="form-control" id="model" name="model">
 
-                <label for="engine" class="form-label">Engine</label>
+                <label for="engine" class="form-label">Mootor</label>
                 <input type="text" class="form-control" id="engine" name="engine">
 
-                <label for="fuel" class="form-label">Fuel</label>
+                <label for="fuel" class="form-label">Kütus</label>
                 <input type="text" class="form-control" id="fuel" name="fuel">
 
-                <label for="price" class="form-label">Price</label>
+                <label for="price" class="form-label">Hind</label>
                 <input type="text" class="form-control" id="price" name="price">
             </div> 
             <div class="col-sm-6">
-                
+                <label for="year" class="form-label">Aasta</label>
+                <input type="number" class="form-control" id="year" name="year" value="2000">
+
+                <label for="transmission" class="form-label">Käigukast</label>
+                <input type="text" class="form-control" id="transmission" name="transmission" value="automaat">
+
+                <label for="seats" class="form-label">Istmete arv</label>
+                <input type="number" class="form-control" id="seats" name="seats" value="5">
+
+                <label for="description" class="form-label">Muu info</label>
+                <input type="text" class="form-control" id="description" name="description" value="test">
+
+                <label for="status" class="form-label">Olek</label>
+                <input type="text" class="form-control" id="status" name="status" value="vaba">
+           </div>     
        </div>
         <input type="submit" value="Salvesta" class="btn btn-success">
 
