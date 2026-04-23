e4a9a088e35c235331a2b3446222e8809671d871
sjakobson <sjakobson@hkhk.edu.ee>
2026-04-23 14:29:52 +0300

    89

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if (!isset($_SESSION['tuvastamine'])) {
+  header('Location: login.php');
+  exit();
+  }
+?>
 <?php include('../config.php'); ?>
 <?php include('../header.php'); ?>
 
