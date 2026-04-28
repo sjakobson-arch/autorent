8ec2143b077d7252642902fd540b98a5ea0afd52
sjakobson-arch <sjakobson@hkhk.edu.ee>
2026-04-28 11:02:20 +0300

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -1,5 +1,5 @@
-<?php
-    session_start();
+<?php 
+    session_start(); 
     include('../config.php');
 ?>
 <!doctype html>
@@ -11,52 +11,57 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   </head>
   <body>
-  <?php
+<?php
     $msg = "";
 
     if (!empty($_POST)) {
+
+        //kasutaja vormist
         $uname = $_POST['user'];
         $password = $_POST['password'];
 
         //kasutaja andmebaasist
-        $paring = "SELECT user, password FROM users WHERE user";
+        $paring = "SELECT user, password FROM users WHERE user='".$uname."'";
+        print_r($paring);
         $valjund = mysqli_query($yhendus, $paring);
         $rida = mysqli_fetch_assoc($valjund);
-        // var_dump($rida);
 
-        //$hash = '$2y$10$dgwQy4wd1TAmLtoYGSi2QeCnxoptLYA/u2s8x.SDdCXutqIhYHEUm';
+        if (!empty($rida)) {
+            $hash = $rida['password'];
+            if ($uname== $rida['user'] && password_verify($password, $hash)) {
+                $_SESSION['tuvastamine'] = 'misiganes';
+                header("Location: index.php");
+            }else{
+                $msg = "kasutaja vale";
+            }
+        }
 
-        if ($uname==$rida['user'] && password_verify($password, $hash)) {
-            $_SESSION['tuvastamine'] = 'misiganes';
-            header("Location: index.php");
-        }else{
-            $msg = "kasutaja vale";
+    }
 
-        }    
-    } 
-  ?>
-  </body>
+?>
     <div class="container">
-        <div class="row">
+        <div class="row pt-4 mt-4">
             <div class="col-sm-4"></div>
             <div class="col-sm-4">
-              <form method="post" action="login.php">
-                <div class="mb-3">
-                    <label for="u" class="form-label">Username</label>
-                    <input name="user" type="text" class="form-control" id="u">
-                </div>
-                <div class="mb-3">
-                    <label for="p" class="form-label">Password</label>
-                    <input name="password" type="password" class="form-control" id="p">
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-             </form>
-             <?php echo $msg; ?>
-           </div>
-           <div class="col-sm-4"></div>
+                <form method="post" action="login.php" autocomplete="off">
+                    <div class="mb-3">
+                        <label for="u" class="form-label">Username</label>
+                        <input name="user" type="text" class="form-control" id="u">
+                    </div>
+                    <div class="mb-3">
+                        <label for="p" class="form-label">Password</label>
+                        <input name="password" type="password" class="form-control" id="p" >
+                    </div>
+                    <button type="submit" class="btn btn-primary">Logi sisse</button>
+                </form>
+                <?php echo $msg; ?>
+            </div>
+            <div class="col-sm-4"></div>
         </div>
     </div>
-   
+  
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
   </body>
 </html>
