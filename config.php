63c0e10c7aa1ef6a28ae0d86d048863e603d02f1
sjakobson-arch <sjakobson@hkhk.edu.ee>
2026-04-28 12:28:33 +0300

    ss

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
     // ***protseduuriline***
     // sinu andmed
-    $db_server = 'localhost';
+    $db_server = 'db';
     $db_andmebaas = 'autorent';
     $db_kasutaja = 'root';
     $db_salasona = 'rootpass';
