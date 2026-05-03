36b5ba01e03732123739787c05ea4a9063a3a1b6
sjakobson-arch <sjakobson@hkhk.edu.ee>
2026-05-03 16:44:17 +0300

    cdcd

--- registreeru.php
+++ registreeru.php
@@ -8,14 +8,16 @@ $vead = "";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
+    $first_name = trim($_POST["first_name"]);
+    $last_name = trim($_POST["last_name"]);
     $email = trim($_POST["email"]);
     $password = $_POST["password"];
     $password2 = $_POST["password2"];
 
-    // Kontroll: kas paroolid kattuvad
     if ($password !== $password2) {
         $vead = "Paroolid ei kattu.";
     } else {
+
         // Kontroll: kas email on juba kasutusel
         $paring = "SELECT * FROM users WHERE email='$email'";
         $valjund = mysqli_query($yhendus, $paring);
@@ -23,11 +25,13 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         if (mysqli_num_rows($valjund) > 0) {
             $vead = "Selline email on juba registreeritud.";
         } else {
-            // Loo uus kasutaja
+
+            // Hashime parooli
             $hashed = password_hash($password, PASSWORD_DEFAULT);
 
-           $paring = "INSERT INTO users (email, password, role)
-           VALUES ('$email', '$hashed', 'client')";
+            // ÕIGE INSERT tabeli struktuuri järgi
+            $paring = "INSERT INTO users (role, first_name, last_name, email, password_hash)
+                       VALUES ('customer', '$first_name', '$last_name', '$email', '$hashed')";
 
             mysqli_query($yhendus, $paring);
 
@@ -48,9 +52,15 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <?php endif; ?>
 
     <form method="POST">
+
         <div class="mb-3">
-            <label class="form-label">Kasutajanimi</label>
-            <input type="text" name="username" class="form-control" required>
+            <label class="form-label">Eesnimi</label>
+            <input type="text" name="first_name" class="form-control" required>
+        </div>
+
+        <div class="mb-3">
+            <label class="form-label">Perekonnanimi</label>
+            <input type="text" name="last_name" class="form-control" required>
         </div>
 
         <div class="mb-3">
@@ -74,3 +84,4 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
 </body>
 </html>
+
