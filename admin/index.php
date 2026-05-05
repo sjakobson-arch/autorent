9cfc2159eb2080bf672d373a6b5a33753b793407
sjakobson-arch <sjakobson@hkhk.edu.ee>
2026-05-05 13:34:20 +0300

    vvvvvvvvvv

--- admin/index.php
+++ admin/index.php
@@ -20,19 +20,10 @@ $broneeringud_tuhistatud = mysqli_fetch_assoc(mysqli_query($yhendus, "SELECT COU
         <!-- Autode haldus -->
         <div class="col-md-4">
             <div class="card shadow-sm mb-4">
-                <div class="card-body d-flex align-items-center">
-
-                    <!-- UUS AUTO IKoon -->
-                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="#0d6efd" class="me-3" viewBox="0 0 16 16">
-                        <path d="M4 9a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm10 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
-                        <path d="M0 7l1-2h14l1 2v5h-1a2 2 0 1 0-4 0H6a2 2 0 1 0-4 0H1V7z"/>
-                    </svg>
-
-                    <div>
-                        <h4 class="card-title">Autode haldus</h4>
-                        <p class="card-text">Lisa, muuda või kustuta autosid.</p>
-                        <a href="cars.php" class="btn btn-primary w-100">Halda autosid</a>
-                    </div>
+                <div class="card-body">
+                    <h4 class="card-title">Autode haldus</h4>
+                    <p class="card-text">Lisa, muuda või kustuta autosid.</p>
+                    <a href="cars.php" class="btn btn-primary w-100">Halda autosid</a>
                 </div>
             </div>
         </div>
@@ -40,19 +31,10 @@ $broneeringud_tuhistatud = mysqli_fetch_assoc(mysqli_query($yhendus, "SELECT COU
         <!-- Broneeringute haldus -->
         <div class="col-md-4">
             <div class="card shadow-sm mb-4">
-                <div class="card-body d-flex align-items-center">
-
-                    <!-- UUS KALENDRI IKoon -->
-                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="#0d6efd" class="me-3" viewBox="0 0 16 16">
-                        <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v1H0V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5z"/>
-                        <path d="M16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2z"/>
-                    </svg>
-
-                    <div>
-                        <h4 class="card-title">Broneeringud</h4>
-                        <p class="card-text">Vaata ja halda kõiki broneeringuid.</p>
-                        <a href="reservations.php" class="btn btn-primary w-100">Halda broneeringuid</a>
-                    </div>
+                <div class="card-body">
+                    <h4 class="card-title">Broneeringud</h4>
+                    <p class="card-text">Vaata ja halda kõiki broneeringuid.</p>
+                    <a href="reservations.php" class="btn btn-primary w-100">Halda broneeringuid</a>
                 </div>
             </div>
         </div>
