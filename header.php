5d82af29f6eb1f6d5ddd3429d473ce79caa69f1f
sjakobson-arch <sjakobson@hkhk.edu.ee>
2026-05-05 13:10:56 +0300

    yyrt

--- header.php
+++ header.php
@@ -1,57 +1,116 @@
-<?php if (session_status() === PHP_SESSION_NONE) session_start(); ?>
-<!doctype html>
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+
+<!DOCTYPE html>
 <html lang="et">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="UTF-8">
     <title>Autorent</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
-  </head>
-  <body>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary mb-4">
-  <div class="container">
-    <a class="navbar-brand" href="index.php">Autorent</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <a class="navbar-brand" href="/autorent/index.php">Autorent</a>
 
-<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <div class="collapse navbar-collapse">
 
-    <?php if (!isset($_SESSION["user_id"])): ?>
-        <li class="nav-item">
-            <a class="nav-link" href="login.php">Logi sisse</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="registreeru.php">Registreeru</a>
-        </li>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <?php if (isset($_SESSION["user_id"])): ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/autorent/my_rentals.php">Minu rendid</a>
+                    </li>
+                <?php endif; ?>
 
-    <?php else: ?>
-        <li class="nav-item">
-            <a class="nav-link" href="minu_rendid.php">Minu rendid</a>
-        </li>
+                <?php if (isset($_SESSION["role"]) && $_SESSION["role"] === "admin"): ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/autorent/admin/index.php">Admin</a>
+                    </li>
+                <?php endif; ?>
+            </ul>
 
-        <li class="nav-item">
-            <span class="nav-link disabled">
-             Tere, <?php echo $_SESSION["first_name"]; ?>
-            </span>
+            <!-- OTSING (AUTOCOMPLETE + PILTIDEGA) -->
+            <form class="d-flex position-relative" method="GET" action="/autorent/index.php" autocomplete="off">
+                <input id="searchInput" 
+                       class="form-control" 
+                       type="search" 
+                       name="otsi" 
+                       placeholder="Otsi autot...">
 
-        </li>
+                <div id="searchResults" 
+                     class="list-group position-absolute w-100" 
+                     style="top: 40px; z-index: 2000;"></div>
+            </form>
 
-        <li class="nav-item">
-            <a class="btn btn-danger ms-2" href="logout.php">Logi välja</a>
-        </li>
-    <?php endif; ?>
+            <!-- PAREM POOL -->
+            <ul class="navbar-nav ms-3">
+                <?php if (isset($_SESSION["user_id"])): ?>
+                    <li class="nav-item">
+                        <span class="nav-link">Tere, <?php echo $_SESSION["first_name"]; ?></span>
+                    </li>
+                    <li class="nav-item">
+                        <a class="btn btn-danger" href="/autorent/logout.php">Logi välja</a>
+                    </li>
+                <?php else: ?>
+                    <li class="nav-item">
+                        <a class="btn btn-success" href="/autorent/login.php">Logi sisse</a>
+                    </li>
+                <?php endif; ?>
+            </ul>
 
-</ul>
-
-      <form class="d-flex ms-3" role="search" method="get" action="index.php">
-        <input class="form-control me-2" type="search" placeholder="Search" name="otsi">
-        <button class="btn btn-outline-success" type="submit">Otsi</button>
-      </form>
-
+        </div>
     </div>
-  </div>
 </nav>
+
+<script>
+// AUTOCOMPLETE
+document.getElementById("searchInput").addEventListener("input", function() {
+    let query = this.value;
+
+    if (query.length < 1) {
+        document.getElementById("searchResults").innerHTML = "";
+        return;
+    }
+
+    fetch("/autorent/search_autocomplete.php?term=" + query)
+        .then(response => response.json())
+        .then(data => {
+            let results = document.getElementById("searchResults");
+            results.innerHTML = "";
+
+            data.forEach(item => {
+                let a = document.createElement("a");
+                a.classList.add("list-group-item", "list-group-item-action", "d-flex", "align-items-center");
+                a.style.cursor = "pointer";
+
+                // Pilt
+                let img = document.createElement("img");
+                img.src = item.image;
+                img.style.width = "50px";
+                img.style.height = "40px";
+                img.style.objectFit = "cover";
+                img.classList.add("me-2");
+
+                // Tekst
+                let span = document.createElement("span");
+                span.textContent = item.name;
+
+                a.appendChild(img);
+                a.appendChild(span);
+
+                a.onclick = () => {
+                    document.getElementById("searchInput").value = item.name;
+                    results.innerHTML = "";
+                };
+
+                results.appendChild(a);
+            });
+        });
+});
+</script>
+
