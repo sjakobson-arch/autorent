3815eb7a71db16a7460726471b310b338cc7ca76
sjakobson-arch <sjakobson@hkhk.edu.ee>
2026-05-05 11:52:42 +0300

    ilili

--- admin/index.php
+++ admin/index.php
@@ -1,93 +1,54 @@
-<?php include("admin_check.php"); ?>
 <?php
-session_start();
+// Admini ligipääsu kontroll
+include("admin_check.php");
 
-if (!isset($_SESSION['tuvastamine'])) {
-  header('Location: login.php');
-  exit();
-  }
+// Andmebaasi ühendus
+include("../config.php");
 
+// Ühine header (navbar jne)
+include("../header.php");
 ?>
 
-<?php include('../config.php'); ?>
-<?php include('../header.php'); ?>
+<div class="container mt-5">
+    <h1 class="mb-4">Admini juhtpaneel</h1>
 
-<!-- sisu -->
-<div class="container">
-    <h2>Adminni ala</h2>
-    <a href="lisa.php" class="btn btn-success">+ Lisa auto</a>
-    <div class="row row-cols-1 row-cols-md-4 g-4">
-<!-- üks auto -->
-<?php
-    // sõnumi kuvamine
-    if(isset($_GET['msg'])){
-      echo '<div class="alert alert-success" role="alert"> Kõik on hästi! </div>';
-    }
+    <div class="row">
 
+        <!-- Autode haldus -->
+        <div class="col-md-4">
+            <div class="card shadow-sm mb-4">
+                <div class="card-body">
+                    <h4 class="card-title">Autode haldus</h4>
+                    <p class="card-text">Lisa, muuda või kustuta autosid.</p>
+                    <a href="cars.php" class="btn btn-primary w-100">Halda autosid</a>
+                </div>
+            </div>
+        </div>
 
-  //autode kuvamine
-    $paring = "SELECT * FROM cars";
-    if (!empty($_GET["otsi"])) {
-        $otsing = $_GET["otsi"];
-        $paring .= " WHERE mark LIKE '%".$otsing."%'";
-    } 
-    $paring .= " LIMIT 8";
-    // var_dump($_GET["otsi"]);
+        <!-- Broneeringute haldus -->
+        <div class="col-md-4">
+            <div class="card shadow-sm mb-4">
+                <div class="card-body">
+                    <h4 class="card-title">Broneeringud</h4>
+                    <p class="card-text">Vaata ja halda kõiki broneeringuid.</p>
+                    <a href="reservations.php" class="btn btn-primary w-100">Halda broneeringuid</a>
+                </div>
+            </div>
+        </div>
 
-    $valjund = mysqli_query($yhendus, $paring); //saadan päringu andmebaasi
+        <!-- Kasutajate haldus -->
+        <div class="col-md-4">
+            <div class="card shadow-sm mb-4">
+                <div class="card-body">
+                    <h4 class="card-title">Kasutajad</h4>
+                    <p class="card-text">Vaata kasutajaid ja muuda rolle.</p>
+                    <a href="users.php" class="btn btn-primary w-100">Halda kasutajaid</a>
+                </div>
+            </div>
+        </div>
 
-?>
-
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Mark</th>
-      <th scope="col">Mudel</th>
-      <th scope="col">Hind</th>
-      <th scope="col">Hind</th>
-      <th scope="col">Hind</th>
-      <th scope="col">Hind</th>
-      <th scope="col">Hind</th>
-      <th scope="col">Hind</th>
-      <th scope="col">Hind</th>
-      <th scope="col">Hind</th>
-      <th scope="col">Hind</th>
-      <th scope="col">Kustuta</th>
-      <th scope="col">Muuda</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
-        while($rida = mysqli_fetch_assoc($valjund)){       //sikutan vastuse alla
-            // var_dump($rida);                       //kuvan testvastuse
-    ?>
-    <tr>
-      <th scope="row"><?php echo $rida["id"]; ?></th>
-      <td><?php echo $rida["mark"]; ?></td>
-      <td><?php echo $rida["model"]; ?></td>
-      <td><?php echo $rida["price"]; ?></td>
-      <td><?php echo $rida["price"]; ?></td>
-      <td><?php echo $rida["price"]; ?></td>
-      <td><?php echo $rida["price"]; ?></td>
-      <td><?php echo $rida["price"]; ?></td>
-      <td><?php echo $rida["price"]; ?></td>
-      <td><?php echo $rida["price"]; ?></td>
-      <td><?php echo $rida["price"]; ?></td>
-      <td><?php echo $rida["price"]; ?></td>
-      <td><a href="kustuta.php?delid=<?= $rida["id"]; ?>" class="btn btn-danger">Kustuta</a></td>
-      <td><a href="muuda.php?editid=<?= $rida["id"]; ?>" class="btn btn-warning">Muuda</a></td>
-    </tr>
-
-    <?php } ?>
-
-  </tbody>
-</table>
-
+    </div>
 </div>
-<!-- /sisu -->
 
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-  </body>
+</body>
 </html>
