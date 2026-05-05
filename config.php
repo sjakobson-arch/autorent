fde5064bcf03f0a233f37512361d41eef623cd6f
sjakobson-arch <sjakobson@hkhk.edu.ee>
2026-05-05 15:11:20 +0300

    czzzzzzc

--- config.php
+++ config.php
@@ -1,12 +1,4 @@
 <?php
-session_start([
-    'cookie_httponly' => true,
-    'cookie_secure' => isset($_SERVER['HTTPS']),
-    'cookie_samesite' => 'Strict'
-]);
-?>
-
-<?php
 $server = "localhost";
 $kasutaja = "autorent";
 $parool = "Parool123!";
