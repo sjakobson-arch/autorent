2da00d072e1e897708bd0cd1b3bce824b9056c82
sjakobson-arch <sjakobson@hkhk.edu.ee>
2026-05-07 17:02:31 +0300

    bfbfb

--- config.php
+++ config.php
@@ -1,14 +1,29 @@
 <?php
+// Vaikimisi väärtused (Ubuntu server / tavaline install)
+$db_server    = 'localhost';
+$db_andmebaas = 'autorent';
+$db_kasutaja  = 'autorent';
+$db_salasona  = 'Parool123!';
 
-// Kui Dockeris, siis DB_HOST= db
-$host = getenv('DB_HOST') ?: 'localhost';
-$db   = getenv('DB_NAME') ?: 'autorent';
-$user = getenv('DB_USER') ?: 'root';
-$pass = getenv('DB_PASSWORD') ?: '';
+// Kui Dockeris on keskkonnamuutujad määratud, kirjutame need üle
+if (getenv('DB_HOST')) {
+    $db_server = getenv('DB_HOST');
+}
+if (getenv('DB_NAME')) {
+    $db_andmebaas = getenv('DB_NAME');
+}
+if (getenv('DB_USER')) {
+    $db_kasutaja = getenv('DB_USER');
+}
+if (getenv('DB_PASSWORD')) {
+    $db_salasona = getenv('DB_PASSWORD');
+}
 
-$mysqli = new mysqli($host, $user, $pass, $db);
+// Ühendus andmebaasiga
+$yhendus = mysqli_connect($db_server, $db_kasutaja, $db_salasona, $db_andmebaas);
 
-if ($mysqli->connect_error) {
-    die("Database connection failed: " . $mysqli->connect_error);
+// Ühenduse kontroll
+if (!$yhendus) {
+    die('Ei saa ühendust andmebaasiga');
 }
 ?>
